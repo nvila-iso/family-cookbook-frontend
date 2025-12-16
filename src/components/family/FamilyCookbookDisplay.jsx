f0eb9b4b0f1e6625fd3e330a07a92da8957d41cf
nvila-iso <nvila88@proton.me>
2025-12-16 15:15:22 -0500

    family style changes

--- src/components/family/FamilyCookbookDisplay.jsx
+++ src/components/family/FamilyCookbookDisplay.jsx
@@ -1,8 +1,12 @@
 import CommonButton from "../commonUI/CommonButton";
 import { IoGridSharp } from "react-icons/io5";
 import { PiListLight } from "react-icons/pi";
+import { FaRegClock } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa6";
+import { BiBowlRice } from "react-icons/bi";
 
 const FamilyCookbookDisplay = ({ family }) => {
+  console.log(family);
   return (
     <>
       <div className="border border-black/30 rounded p-2">
@@ -44,7 +48,34 @@ const FamilyCookbookDisplay = ({ family }) => {
         </div>
 
         {/* ACTUAL DISPLAY */}
-        <div className="flex flex-col justify-center items-center h-full"></div>
+        <div className="flex flex-col justify-center items-center h-full">
+          <div className="w-52 h-60 border border-black/10 shadow-xs rounded-lg p-2 flex flex-col gap-1 hover:shadow-md transition">
+            <img
+              src="https://hips.hearstapps.com/hmg-prod/images/lentil-soup-recipe-2-677c54158ad10.jpg?crop=0.6667718689179948xw:1xh;center,top&resize=1200:*"
+              alt=""
+              className="h-32 w-full rounded-lg object-cover"
+            />
+            <div className="text-xs flex justify-between">
+              <div className="flex gap-1 items-center">
+                <FaRegClock />
+                <p>30 min</p>
+              </div>
+              <div className="flex gap-1 items-center">
+                <BiBowlRice />
+                <p>4 Servings</p>
+              </div>
+            </div>
+            <p className="font-bold text-lg text-zinc-600">Lentil Soup</p>
+            <p className="text-xs text-zinc-500">Good soup, good poop.</p>
+            <div className="flex flex-1 justify-between items-center text-xs">
+              <p>added by </p>
+              <div className="flex gap-1 items-center">
+                <FaRegHeart />
+                <p className="text-zinc-600">102</p>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
--- src/components/family/FamilyProfileSidePanel.jsx
+++ src/components/family/FamilyProfileSidePanel.jsx
@@ -1,88 +1,94 @@
 import { Link } from "react-router";
+import { MdPeopleAlt } from "react-icons/md";
+import { BsPostcardHeartFill } from "react-icons/bs";
+import { RiUserFollowFill } from "react-icons/ri";
+import { FaGear } from "react-icons/fa6";
+import star from "../../assets/star.svg";
 
 const FamilyProfileSidePanel = ({ family, user }) => {
   const isFamilyMember = user?.family?.id === family?.id;
 
-
   return (
     <>
-      <div className="flex flex-col border border-black/30 h-full rounded gap-3">
-        {/* FAMILY DETAILS */}
-        {/* STATS */}
-
-        <div className="grid grid-cols-3 p-1 bg-amber-100">
-          {/* MEMBERS */}
-          <div className="flex flex-col justify-center items-center text-xs">
-            <p>{family?.members?.length}</p>
-            <p>members</p>
+      <div className="h-[60vh] p-2 border rounded-3xl bg-amber-100/20 border-amber-200 shadow-md">
+        <div className="p-2 h-full border-2 border-dashed border-amber-200 rounded-2xl grid grid-rows-[1fr_60px_1fr_38px] gap-2">
+          <div className="flex flex-col justify-center items-center relative overflow-hidden">
+            <div className="border border-white border-5 flex justify-center items-center mx-auto w-30 h-30 rounded-full bg-teal-100 text-lime-700 text-[5rem] text-center font-semibold shadow-xs">
+              <p>{family?.name?.[0]?.toUpperCase() || "U"}</p>
+            </div>
+            <p className="text-3xl text-center italic text-black/60 font-semibold">
+              {family?.name} Family
+            </p>
+            <img src={star} alt="" className="size-7 absolute left-45 bottom-40 rotate-15"/>
+            <img src={star} alt="" className="size-4 absolute left-41 bottom-41 rotate-15"/>
+            <img src={star} alt="" className="size-3 absolute right-48 bottom-25"/>
           </div>
-
-          {/* RECIPES */}
-          <div className="flex flex-col justify-center items-center text-xs">
-            <p>{family?.recipes?.length}</p>
-            <p>recipes</p>
+          <div>
+            <div className="bg-white grid grid-cols-[1fr_1px_1fr_1px_1fr] px-1 py-3 rounded-xl shadow-sm">
+              {/* MEMBERS */}
+              <div className="flex flex-col justify-center items-center text-xs">
+                <MdPeopleAlt className="text-base text-lime-600" />
+                <div className="flex gap-1">
+                  <p>{family?.members?.length}</p>
+                  {family?.members?.length > 1 ? <p>members</p> : <p>member</p>}
+                </div>
+              </div>
+              <div className="w-[1px] h-full bg-black/20"></div>
+              {/* RECIPES */}
+              <div className="flex flex-col justify-center items-center text-xs">
+                <BsPostcardHeartFill className="text-base text-amber-400" />
+                <div className="flex gap-1">
+                  <p>{family?.recipes?.length}</p>
+                  {family?.recipes?.length > 1 ? <p>recipes</p> : <p>recipe</p>}
+                </div>
+              </div>
+              <div className="w-[1px] h-full bg-black/20"></div>
+              {/* FOLLOWERS */}
+              <div className="flex flex-col justify-center items-center text-xs">
+                <RiUserFollowFill className="text-base text-purple-400" />
+                <div className="flex gap-1">
+                  <p>?</p>
+                  <p>followers</p>
+                </div>
+              </div>
+            </div>
           </div>
-
-          {/* FOLLOWERS */}
-          <div className="flex flex-col justify-center items-center text-xs">
-            <p>?</p>
-            <p>followers</p>
+          <div>
+            <p>
+              Fusce ac eleifend nisi. Cras maximus malesuada libero, ac
+              consectetur libero volutpat in. Maecenas dolor turpis, accumsan a
+              nulla id, aliquam pretium urna. Sed et elementum tortor.
+              Pellentesque habitant morbi tristique.
+            </p>
           </div>
-        </div>
-
-        {/* FAMILY SEAL / PROFILE IMG */}
-        <div className="flex justify-center items-center mx-auto w-50 h-50 rounded-full bg-teal-100 text-lime-700 text-[10rem] text-center font-semibold">
-          <p>{family?.name?.[0]?.toUpperCase() || "U"}</p>
-        </div>
-
-        {/* FAMILY BIO */}
-        <div className="px-2">
-          <p className="text-3xl text-center underline italic text-black/60 font-semibold">
-            {family?.name}
-          </p>
-          <div className="py-2 w-[55%] grid grid-cols-5 mx-auto">
-            {family?.members?.map((m) => (
-              <p
-                key={m.id}
-                className="h-10 w-10 w-10 text-3xl text-center rounded-full bg-red-200 shadow-xs hover:z-1 hover:scale-110 transition cursor-pointer"
+          <div className="flex justify-center item-center">
+            {isFamilyMember && (
+              <button className="flex justify-center items-center gap-1 bg-white rounded-full w-full shadow-sm text-lg font-semibold text-stone-500">
+                <FaGear className="text-stone-400" /> Settings
+              </button>
+            )}
+            {/* {user && !isFamilyMember ? (
+              <button className="bg-teal-100 text-lime-700 font-bold px-2 py-1 w-32 rounded mx-auto mb-3 hover:bg-teal-200 transition">
+                Follow
+              </button>
+            ) : isFamilyMember ? (
+              <Link
+                to="/settings"
+                state={{ activeTab: "details" }}
+                className="text-center bg-teal-100 text-lime-700 font-bold px-2 py-1 w-32 rounded mx-auto mb-3 hover:bg-teal-200 transition"
               >
-                {m?.firstName?.[0]?.toUpperCase() ||
-                  family?.name?.[0]?.toUpperCase()}
-              </p>
-            ))}
+                Settings
+              </Link>
+            ) : (
+              <Link
+                to="/login"
+                className="text-center bg-teal-100 text-lime-700 font-bold px-2 py-1 w-44 rounded mx-auto mb-3 hover:bg-teal-200 transition"
+              >
+                Login to follow
+              </Link>
+            )} */}
           </div>
-          <p>
-            Fusce ac eleifend nisi. Cras maximus malesuada libero, ac
-            consectetur libero volutpat in. Maecenas dolor turpis, accumsan a
-            nulla id, aliquam pretium urna. Sed et elementum tortor.
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Aliquam erat volutpat. Aliquam quam nibh,
-            lacinia at imperdiet ut, venenatis non ante. Donec at consequat
-            magna. Vivamus lobortis tellus ut mauris lobortis sollicitudin.
-          </p>
         </div>
-
-        {/* FOLLOW BUTTON */}
-        {user && !isFamilyMember ? (
-          <button className="bg-teal-100 text-lime-700 font-bold px-2 py-1 w-32 rounded mx-auto mb-3 hover:bg-teal-200 transition">
-            Follow
-          </button>
-        ) : isFamilyMember ? (
-          <Link
-            to="/settings"
-            state={{ activeTab: "details" }}
-            className="text-center bg-teal-100 text-lime-700 font-bold px-2 py-1 w-32 rounded mx-auto mb-3 hover:bg-teal-200 transition"
-          >
-            Settings
-          </Link>
-        ) : (
-          <Link
-            to="/login"
-            className="text-center bg-teal-100 text-lime-700 font-bold px-2 py-1 w-44 rounded mx-auto mb-3 hover:bg-teal-200 transition"
-          >
-            Login to follow
-          </Link>
-        )}
       </div>
     </>
   );
